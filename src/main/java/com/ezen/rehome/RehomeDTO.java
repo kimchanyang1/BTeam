60425acf4614a8d805d6b1e60889e47408926589
Seulgi Choi <skmiru@naver.com>
2022-05-11 11:36:16 +0900

    분양임보 DTO 쿼리 작성

---
+++ src/main/java/com/ezen/rehome/RehomeDTO.java
@@ -0,0 +1,25 @@
+package com.ezen.rehome;
+
+/*
+create table rehome(
+rh_no NUMBER(6) PRIMARY KEY,
+rh_gb1 VARCHAR2(10),
+rh_gb2 VARCHAR(10),
+rh_gb3 VARCHAR2(10),
+rh_title VARCHAR2(80),
+rh_pname VARCHAR2(20),
+rh_pno NUMBER(20),
+rh_misdate date,
+rh_misplace VARCHAR2(50),
+rh_image VARCHAR2(30),
+mem_no NUMBER(6,0),
+mem_nickname VARCHAR2(20),
+mem_tel VARCHAR2(13),
+rh_writeday DATE default sysdate,
+rh_content VARCHAR2(20),
+rh_readcount NUMBER(6));
+ */
+
+public class RehomeDTO {
+
+}
